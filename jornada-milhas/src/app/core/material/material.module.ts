b40063c8690e18f9ce6197c4d570f8c273f582e1
Jhones <jhonesaly@gmail.com>
2024-03-21 15:27:18 -0300

    fix: remove junk code

--- jornada-milhas/src/app/core/material/material.module.ts
+++ jornada-milhas/src/app/core/material/material.module.ts
@@ -1,20 +1,20 @@
-import { NgModule } from "@angular/core";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { MatButtonModule } from "@angular/material/button";
-import { MatButtonToggleModule } from "@angular/material/button-toggle";
-import { MatCardModule } from "@angular/material/card";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatChipsModule } from "@angular/material/chips";
-import { MatNativeDateModule } from "@angular/material/core";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatDividerModule } from "@angular/material/divider";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatRadioModule } from "@angular/material/radio";
-import { MatSliderModule } from "@angular/material/slider";
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { NgModule } from '@angular/core';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
@@ -36,4 +36,4 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     MatSliderModule
   ]
 })
-export class MaterialModule {}
+export class MaterialModule { }
--- jornada-milhas/src/app/shared/shared.module.ts
+++ jornada-milhas/src/app/shared/shared.module.ts
@@ -1,4 +1,7 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+
 import { BannerComponent } from "./banner/banner.component";
 import { BotaoControleComponent } from "./botao-controle/botao-controle.component";
 import { CardBuscaComponent } from "./card-busca/card-busca.component";
@@ -8,43 +11,27 @@ import { ContainerComponent } from "./container/container.component";
 import { DropdownUfComponent } from "./dropdown-uf/dropdown-uf.component";
 import { FooterComponent } from "./footer/footer.component";
 import { FormBaseComponent } from "./form-base/form-base.component";
-import { CompanhiasComponent } from "./form-busca/filtros-complementares/companhias/companhias.component";
-import { FiltrosComplementaresComponent } from "./form-busca/filtros-complementares/filtros-complementares.component";
-import { LabelComponent } from "./form-busca/filtros-complementares/label/label.component";
-import { ParadasComponent } from "./form-busca/filtros-complementares/paradas/paradas.component";
-import { PrecosComponent } from "./form-busca/filtros-complementares/precos/precos.component";
 import { FormBuscaComponent } from "./form-busca/form-busca.component";
 import { HeaderComponent } from "./header/header.component";
 import { ModalComponent } from "./modal/modal.component";
-import { PassagemDestaqueComponent } from "./passagem-destaque/passagem-destaque.component";
-import { PassagemComponent } from "./passagem/passagem.component";
 import { SeletorPassageiroComponent } from "./seletor-passageiro/seletor-passageiro.component";
-import { CommonModule } from "@angular/common";
 import { MaterialModule } from "../core/material/material.module";
-import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [
-    HeaderComponent,
     BannerComponent,
     CardComponent,
-    ContainerComponent,
-    FooterComponent,
     CardBuscaComponent,
     CardDepoimentoComponent,
-    FormBuscaComponent,
-    ModalComponent,
-    BotaoControleComponent,
+    ContainerComponent,
     DropdownUfComponent,
-    SeletorPassageiroComponent,
+    FooterComponent,
     FormBaseComponent,
-    PassagemComponent,
-    ParadasComponent,
-    CompanhiasComponent,
-    PrecosComponent,
-    LabelComponent,
-    FiltrosComplementaresComponent,
-    PassagemDestaqueComponent
+    FormBuscaComponent,
+    HeaderComponent,
+    SeletorPassageiroComponent,
+    ModalComponent,
+    BotaoControleComponent
   ],
   imports: [
     CommonModule,
@@ -52,26 +39,19 @@ import { ReactiveFormsModule } from "@angular/forms";
     ReactiveFormsModule
   ],
   exports: [
-    HeaderComponent,
     BannerComponent,
     CardComponent,
-    ContainerComponent,
-    FooterComponent,
     CardBuscaComponent,
     CardDepoimentoComponent,
-    FormBuscaComponent,
-    ModalComponent,
-    BotaoControleComponent,
+    ContainerComponent,
     DropdownUfComponent,
-    SeletorPassageiroComponent,
+    FooterComponent,
     FormBaseComponent,
-    PassagemComponent,
-    ParadasComponent,
-    CompanhiasComponent,
-    PrecosComponent,
-    LabelComponent,
-    FiltrosComplementaresComponent,
-    PassagemDestaqueComponent
+    FormBuscaComponent,
+    HeaderComponent,
+    SeletorPassageiroComponent,
+    ModalComponent,
+    BotaoControleComponent
   ]
 })
 export class SharedModule { }
